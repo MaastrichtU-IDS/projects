9df4f1fbccffe331cc939603527e6cb9a633f0e3
Vincent Emonet <vincent.emonet@gmail.com>
2020-10-05 00:32:23 +0200

    add reactive native paper ui

--- App.js
+++ App.js
@@ -1,27 +1,44 @@
 import React from "react";
 import { StyleSheet, Text, View, Platform } from "react-native";
 import { Router, Route, Link } from "./react-router";
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import ProjectsDashboard from "./src/ProjectsDashboard";
 
-const Home = () => <Text>Home</Text>;
+const Home = () => (
+  <Text>Home</Text>
+);
 
 const About = () => <Text>About</Text>;
 
-const App = () => (
-  <Router>
-    <View style={styles.container}>
-      <View style={styles.nav}>
-        <Link to="/">
-          <Text>Home</Text>
-        </Link>
-        <Link to="/about">
-          <Text>About</Text>
-        </Link>
-      </View>
 
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </View>
-  </Router>
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    // primary: '#3498db',
+    // accent: '#f1c40f',
+  },
+};
+
+const App = () => (
+  <PaperProvider>
+    <Router>
+      <View style={styles.container}>
+        <View style={styles.nav}>
+          <Link to="/">
+            <Text>Home</Text>
+          </Link>
+          <Link to="/about">
+            <Text>About</Text>
+          </Link>
+        </View>
+
+        <Route exact path="/" component={ProjectsDashboard} />
+        <Route path="/about" component={About} />
+      </View>
+    </Router>
+  </PaperProvider>
 );
 
 const styles = StyleSheet.create({
