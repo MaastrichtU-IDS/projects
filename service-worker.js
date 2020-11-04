76a48ed15acce0713c5073287fe41b7ebffc8cbe
vemonet <vincent.emonet@gmail.com>
2020-11-04 17:49:35 +0000

    Deploying to gh-pages from  @ 64b7b2d0f6263fccb0e90368cce77785a59653ee 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/projects/precache-manifest.3fdbda0b396066b4d14c38ab183185c6.js"
+  "/projects/precache-manifest.21769318df36e27b4cf075f9fbea57a4.js"
 );
 
 workbox.skipWaiting();
