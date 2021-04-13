bd20b9143ad774f693be0395c689ca2390a46233
vemonet <vincent.emonet@gmail.com>
2021-04-13 20:07:33 +0000

    Deploying to gh-pages from  @ 642d6d8f99c82c23c95817978936eda40f4bea57 🚀

---
+++ precache-manifest.42e9fc6cd90adae56e39148273d66629.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0928af6fb0a1758c0a26",
+    "url": "/projects/static/js/app.0084cd23.chunk.js"
+  },
+  {
+    "revision": "603dcdd999eee7e2d1a0",
+    "url": "/projects/static/js/runtime~app.757d5589.js"
+  },
+  {
+    "revision": "b1f0596ab0b609a87a5b",
+    "url": "/projects/static/js/2.51b53947.chunk.js"
+  },
+  {
+    "revision": "951e6ef1c62c25b9b10b081968119584",
+    "url": "/projects/static/media/icon.72626586.png"
+  },
+  {
+    "revision": "653110a09d3750b255dbe5cfa9f5cf16",
+    "url": "/projects/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/projects/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/projects/expo-service-worker.js"
+  },
+  {
+    "revision": "1a09258382abe5a37c23d38f1a76cdc3",
+    "url": "/projects/register-service-worker.js"
+  },
+  {
+    "revision": "768d9e498c983a310794213d8e59281d",
+    "url": "/projects/static/js/2.51b53947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78221aba04ce077cb705ff3c8d2bc05",
+    "url": "/projects/favicon-16.png"
+  },
+  {
+    "revision": "6a52670e86212233b8d906cc16a59955",
+    "url": "/projects/favicon-32.png"
+  },
+  {
+    "revision": "1deb5f88e9bbfe62f2cd9913b9f9d46e",
+    "url": "/projects/favicon.ico"
+  },
+  {
+    "revision": "697e7b9d38d11ca8995fd7de4b152127",
+    "url": "/projects/manifest.json"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/projects/precache-manifest.6e8081f68275a4412f7d63d54b541476.js"
+  "/projects/precache-manifest.42e9fc6cd90adae56e39148273d66629.js"
 );
 
 workbox.skipWaiting();
